b4dd67bc2ce0d6c5aa13ab019f81aa51f6474a15
Alexander Creznic <alexander.creznic@protonmail.com>
2025-05-08 21:09:14 -0400

    changing project core structure to semi strategy pattern

---
+++ app/services/student_service.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, jsonify, request
+from app.db.db_connection import Connection
+
+class StudentService:
+    # TODO: make this like an abstract class or interface for student API stuff
+    pass
